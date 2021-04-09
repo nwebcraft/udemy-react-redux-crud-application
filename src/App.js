2578d1494ad97bdbf11249c8fd99334290c10c2d
nwebcraft_macbookpro13 <nwebcraft@nwebcraft-macbookpro13.local>
2021-04-09 14:14:44 +0900

    did practice jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 function App() {
-  return (<h1>Hello, React!</h1>);
+  const dom = <h2>Woo!</h2>
+  const input = <input type="text" onChange={() => { console.log('i am changed') }} />
+  return input
 }
 
 export default App;
