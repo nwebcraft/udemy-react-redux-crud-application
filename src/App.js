8ffab55f6280fa1dcff5f2c446243ab9cdfa2e17
nwebcraft_macbookpro13 <nwebcraft@nwebcraft-macbookpro13.local>
2021-04-09 15:08:16 +0900

    did practice prop-types

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import PropTypes from 'prop-types'
+
 const App = () =>  {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "Makoto", age: 46 },
-    { name: "Unknown" },
+    { name: "Unknown", age: 99 }
   ]
 
   return (
@@ -24,8 +26,9 @@ const User = (props) => {
   )
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
